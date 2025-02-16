a2a18503e7a3dbfca7d02e3924e5faf55ae90739
Vansh <vanshgarg2k5@gmail.com>
2025-02-16 16:42:38 +0530

    correction user

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -32,7 +32,7 @@ export const clerkWebhooks = async (req,res) => {
 
             case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
@@ -43,7 +43,7 @@ export const clerkWebhooks = async (req,res) => {
             }
 
             case 'user.deleted': {
-                await User.findByIdAndUpdate(data.id)
+                await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
             }
